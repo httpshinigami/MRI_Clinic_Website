9dab2bf98af6fee366907e313867beaf52101e79
httpshinigami <141820784+httpshinigami@users.noreply.github.com>
2023-09-15 05:23:49 +1000

    work done for FIT5032 Assessment Task 2

---
+++ FIT5032_Assessment_Task2/FIT5032_Assessment_Task2/Controllers/BookingController.cs
@@ -0,0 +1,95 @@
+﻿using FIT5032_Assessment_Task2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_Assessment_Task2.Models;
+
+namespace FIT5032_Assessment_Task2.Controllers
+{
+    public class BookingController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Booking/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Booking/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Booking/Create
+        [HttpPost]
+        public ActionResult Create(Booking model)
+        {
+            try
+            {
+                String DoctorName = model.DoctorName;
+                DateTime BookingTime = model.BookingTime;
+                ViewBag.BookingDetail = DoctorName + " " + BookingTime;
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Booking/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Booking/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Booking/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Booking/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FIT5032_Assessment_Task2/FIT5032_Assessment_Task2/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Assessment_Task2.Models
+{
+    public class Booking
+    {
+        [Required]
+        [Display(Name = "Select a doctor")]
+        public String DoctorName { get; set; }
+        [Display(Name = "Select a date & time")]
+        public DateTime BookingTime { get; set; }
+    }
+}
