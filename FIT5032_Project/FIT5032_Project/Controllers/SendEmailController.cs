96520a0da3e49e04e5ec6220d92d8940e60252ac
httpshinigami <141820784+httpshinigami@users.noreply.github.com>
2023-10-10 03:17:04 +1100

    added email with attachment function

---
+++ FIT5032_Project/FIT5032_Project/Controllers/SendEmailController.cs
@@ -0,0 +1,78 @@
+﻿using FIT5032_Project.Models;
+using FIT5032_Project.Utils;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Project.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Send_Email(SendEmailViewModel model, HttpPostedFileBase Attachment)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string toEmail = model.ToEmail;
+                    string subject = model.Subject;
+                    string contents = model.Contents;
+
+                    byte[] fileBytes = null;
+                    string fileName = null;
+
+                    if (Attachment != null && Attachment.ContentLength > 0)
+                    {
+                        fileName = Path.GetFileName(Attachment.FileName);
+
+                        // Save the attachment to a temporary folder
+                        var path = Server.MapPath("~/Attachments/") + fileName;
+                        Attachment.SaveAs(path);
+
+
+                        // Convert attachment to byte array for SendGrid
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await Attachment.InputStream.CopyToAsync(memoryStream);
+                            fileBytes = memoryStream.ToArray();
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.UploadResult = "File not uploaded.";
+                    }
+
+                    // Send email using SendGrid without an emailMessage object
+                    EmailSender es = new EmailSender();
+                    await es.SendAsync(toEmail, subject, contents, fileBytes, fileName);
+
+                    ViewBag.Result = "Email has been sent.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions appropriately
+                    ViewBag.Error = "An error occurred: " + ex.Message;
+                    return View();
+                }
+            }
+
+            return View();
+
+        }
+
+    }
+
+}
---
+++ FIT5032_Project/FIT5032_Project/Models/SendEmailViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace FIT5032_Project.Models
+{
+    public class SendEmailViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ToEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject.")]
+        [Display(Name = "Subject:")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message")]
+        [Display(Name = "Contents:")]
+        public string Contents { get; set; }
+
+        [Display(Name = "Attachment:")]
+        public HttpPostedFileBase Attachment { get; set; }
+    }
+}
